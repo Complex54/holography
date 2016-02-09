ef579ad66a8ea064a620314e0f6f69807ee1347c
Complex54 <mazzathibault@gmail.com>
2016-02-09 08:40:56 +0100

    modified:   RapportJournalier.txt.txt 	new file:   doc/Rapport journalier Thibault M.txt 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2.sln 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/App.config 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Form1.resx 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Hologram.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Hologram1.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Program.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Properties/AssemblyInfo.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Properties/Resources.Designer.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Properties/Resources.resx 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Properties/Settings.Designer.cs 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/Properties/Settings.settings 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/WindowsFormsApplication2.csproj 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/bite2.PNG 	new file:   lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/virtualScreen.cs

---
+++ lab/PoC virtualScreen Form Display/WindowsFormsApplication2/WindowsFormsApplication2/virtualScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class virtualScreen : IDisposable
+    {
+        private Image image;
+
+        public virtualScreen(string url)
+        {
+            image = pathToImage("C:\\Users\\Thibault\\Pictures\bite2.PNG");
+
+        }
+
+        public Image getImage()
+        {
+            return image;
+        }
+
+        public Image pathToImage(string path)
+        {
+            Image i = Image.FromFile(path);
+            return i;
+        }
+        public void Dispose()
+        {
+            this.Dispose();
+        }
+    }
+}
